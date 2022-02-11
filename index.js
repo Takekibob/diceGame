1db3d9e8c2de2a2badc9ec0bc76fc7ba1a106719
Takekibob <bob19d5104012f@gmail.com>
2022-02-11 21:10:21 +0900

    commit 2/11

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 function dicegame(){
-  var player1Random = Math.random();
-  var player2Random = Math.random();
-  var player1Num = player1Random * 6 + 1;
-  var player2Num = player2Random * 6 + 1;
-  var player1NumFloor = Math.floor(player1Num);
-  var player2NumFloor = Math.floor(player2Num);
-  var i;
+  // input random number
+  var player1Num = Math.floor( Math.random() * 6 + 1 );
+  var player2Num = Math.floor( Math.random() * 6 + 1 );
+
+  // change img
   var img1 = document.querySelector(".img1")
   var img2 = document.querySelector(".img2")
+  img1.src = "images/dice" + player1Num + ".png";
+  img2.src = "images/dice" + player2Num + ".png";
+
+  // change heading text
   var headingText = document.querySelector(".container h1")
-  for(i=1;i<=6;i++){
-    if(player1NumFloor === i){
-      img1.src = "images/dice" + i+ ".png";
-    }
-    if(player2NumFloor === i){
-      img2.src = "images/dice" + i+ ".png";
-    }
-  }
-  console.log(player1NumFloor, player2NumFloor)
-  if(player1NumFloor > player2NumFloor){
+  if(player1Num > player2Num){
     headingText.innerHTML = "🚩 Player1 Win"
   }
-  else if(player1NumFloor === player2NumFloor){
+  else if(player1Num === player2Num){
     headingText.innerHTML = "Draw"
   }
-  else if(player1NumFloor < player2NumFloor){
+  else if(player1Num < player2Num){
     headingText.innerHTML = "Player2 Win 🚩"
   }
 }
